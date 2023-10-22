3a3fc5e75e17c06b92c0a20169be4628b7d8a0c6
Paras Lakhani <lakparas@gmail.com>
2023-10-22 13:19:13 +0530

    adding linting and formatting

---
+++ blog/blogpost/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+# Create your models here.
+
+def upload_to(instance, filename):
+    return 'images/{filename}'.format(filename=filename)
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    image_url = models.ImageField(upload_to=upload_to, blank=True, null=True)
+
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return(self.title)
+
