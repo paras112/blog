a25ddcc11e6fe1e2259e35ed34a983160a9e7256
Paras Lakhani <lakparas@gmail.com>
2023-10-22 13:23:36 +0530

    add untracked files

--- blogpost/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Post
-class PostSerializer(serializers.ModelSerializer):
-    image_url = serializers.ImageField(required=False)
-    class Meta:
-        model = Post
-        fields = ['id', 'author','title','text','created_date','image_url']
